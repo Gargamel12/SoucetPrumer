1bc2e50722de3d7d939c1c58bd652905c5f87bcc
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-11-23 09:26:55 +0100

    zase trošku víc fukční

--- SoucetPrumer/Program.cs
+++ SoucetPrumer/Program.cs
@@ -9,6 +9,7 @@ namespace SoucetPrumer
         {
             ConsoleKeyInfo keyInfo;
             string LeCisla = "0123456789";
+            string text = "";
             do
             {
                 keyInfo = Console.ReadKey(true); //Zachycení stisknuté klávesy
@@ -16,16 +17,22 @@ namespace SoucetPrumer
                 {
                     //když uživatel zmáčkne Enter
                     Console.WriteLine();
+                    Console.WriteLine(text);
+                    text = "";
                 }
                 else if (keyInfo.Key == ConsoleKey.Backspace)
                 {
                     //Když uživatel zmáčkne backspace
-
+                    text = text.Substring(0,text.Length-1);
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(text);
                 }
                 else if (LeCisla.Contains(keyInfo.KeyChar))
                 {
                     //Když uživatel napsal číslici
-                    Console.Write(keyInfo.KeyChar);
+                    text = text + keyInfo.KeyChar;
+                    Console.SetCursorPosition(0,0);
+                    Console.Write(text);
                 }
             } while (keyInfo.Key != ConsoleKey.Escape);
 
